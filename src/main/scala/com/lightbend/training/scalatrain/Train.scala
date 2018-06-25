78e878d074a8b9725262cea94a2bee30aa5a1b24
David Fernandez <david.fernandez.jimenez@ing.com>
2018-06-25 17:50:50 +0200

    A new attribute added to the Train class

--- src/main/scala/com/lightbend/training/scalatrain/Train.scala
+++ src/main/scala/com/lightbend/training/scalatrain/Train.scala
@@ -1,7 +1,10 @@
 package com.lightbend.training.scalatrain
 
-class Train(schedule: Seq[Station]) {
+class Train(val schedule: Seq[(Time, Station)]) {
 
   require(schedule.size >= 2)
+  private val stations : Seq[Station] = schedule.map(timeAndStation => timeAndStation._2)
+
+  //TODO Verify that schedule is strictly increasing in time
 
 }
