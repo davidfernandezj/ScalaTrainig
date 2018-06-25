44617403f771ca5977daa11c76be5f76336d61a9
David Fernandez <david.fernandez.jimenez@ing.com>
2018-06-25 16:15:40 +0200

    Some code has been added

---
+++ src/main/scala/com/lightbend/training/scalatrain/Time.scala
@@ -0,0 +1,27 @@
+package com.lightbend.training.scalatrain
+
+object Time {
+
+  private val minutesPerHours : Int = 60
+  private val hoursPerDay : Int = 24
+
+  def fromMinutes(minutes: Int) : Time = {
+    new Time(minutes / minutesPerHours, minutes % minutesPerHours)
+  }
+
+}
+
+class Time(val hours:Int = 0, val minutes:Int = 0) {
+
+  require(hours >= 0 && hours < Time.hoursPerDay, "The hours should be between 0 and 23")
+  require(minutes >=0 && minutes < Time.minutesPerHours, "The minutes should be between 0 and 59")
+
+  def asMinutes : Int = (hours * Time.minutesPerHours) + minutes
+
+  def minus(other: Time) : Int = this.asMinutes - other.asMinutes
+
+  def -(other: Time) : Int = this.minus(other)
+
+}
+
+
